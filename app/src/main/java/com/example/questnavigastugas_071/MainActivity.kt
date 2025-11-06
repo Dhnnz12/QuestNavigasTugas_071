445ae0653f0c246c7c0e5ed2200eabb14c6d9e60
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-07 00:46:46 +0700

    Menambahkan MainActivity sebagai entry point aplikasi dengan pemanggilan Navigasiku()

--- app/src/main/java/com/example/questnavigastugas_071/MainActivity.kt
+++ app/src/main/java/com/example/questnavigastugas_071/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuestNavigasTugas_071Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Navigasiku()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasTugas_071Theme {
-        Greeting("Android")
-    }
-}

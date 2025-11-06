2ee724e12852852f88af77615d6e8fee32e08db6
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-07 00:26:30 +0700

    Implementasi Data layout dengan background gradient

--- app/src/main/java/com/example/questnavigastugas_071/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_071/view/TampilData.kt
@@ -1,9 +1,17 @@
 package com.example.questnavigastugas_071.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.questnavigastugas_071.R
 
@@ -13,6 +21,16 @@ fun TampilData(navController: NavHostController) {
     val gradient = Brush.verticalGradient(
         colors = listOf(Color(0xFFBB86FC), Color(0xFFEFB8C8))
     )
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .background(gradient)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        }
+    }
 
 
 

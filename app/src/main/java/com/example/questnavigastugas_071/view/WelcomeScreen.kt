ee77296411d8334f4b89974189c2991e4449d8b8
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-07 00:43:22 +0700

    perubahan size di dalam file welcomescreen

--- app/src/main/java/com/example/questnavigastugas_071/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_071/view/WelcomeScreen.kt
@@ -31,20 +31,20 @@ fun WelcomeScreen(navController: NavHostController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Selamat Datang", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+            Text("Selamat Datang", fontSize = 30.sp, fontWeight = FontWeight.Bold)
 
             Spacer(modifier = Modifier.height(24.dp))
 
             Image(
                 painter = painterResource(id = R.drawable.cardlist_logo),
                 contentDescription = "Logo",
-                modifier = Modifier.size(150.dp)
+                modifier = Modifier.size(200.dp)
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text("Hibrizi Fathin Dhonan", fontSize = 18.sp)
-            Text("20230140071", fontSize = 14.sp,
+            Text("Hibrizi Fathin Dhonan", fontSize = 25.sp)
+            Text("20230140071", fontSize = 23.sp,
                 color = MaterialTheme.colorScheme.secondary)
 
             Spacer(modifier = Modifier.height(32.dp))

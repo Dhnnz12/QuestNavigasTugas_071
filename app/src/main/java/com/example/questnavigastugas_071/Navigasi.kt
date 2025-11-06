c3a2b2e324fea11798e2a794a0b43fc22c6002a8
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-07 00:15:03 +0700

    Tambahkan Function WelcomeScreen

--- app/src/main/java/com/example/questnavigastugas_071/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_071/Navigasi.kt
@@ -5,12 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-// Pastikan Anda sudah meng-import file Composable lainnya
-// import com.example.questnavigastugas_071.ui.WelcomeScreen
-// import com.example.questnavigastugas_071.ui.TampilData
-// import com.example.questnavigastugas_071.ui.Formulir
 
-// Menggunakan Enum untuk rute adalah praktik yang baik untuk menghindari kesalahan ketik
 enum class PengelolaHalaman {
     Welcome,
     ListPeserta,
@@ -22,7 +17,7 @@ fun Navigasiku(navController: NavHostController = rememberNavController()) {
 
     NavHost(
         navController = navController,
-        startDestination = PengelolaHalaman.Welcome.name // Menggunakan Enum sebagai startDestination
+        startDestination = PengelolaHalaman.Welcome.name 
     ){
         composable(route = PengelolaHalaman.Welcome.name) {
         }
---
+++ app/src/main/java/com/example/questnavigastugas_071/view/WelcomeScreen.kt
@@ -0,0 +1,6 @@
+package com.example.questnavigastugas_071.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WelcomeScreen(navController: NavHostController) {

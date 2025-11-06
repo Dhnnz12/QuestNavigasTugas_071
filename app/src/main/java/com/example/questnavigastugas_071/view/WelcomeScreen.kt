b5b174cf3e1d3e367d24fe93c4dedeef510c2dca
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-07 00:20:26 +0700

    MEnambahkan spacer dan image buat welcome screen

--- app/src/main/java/com/example/questnavigastugas_071/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_071/view/WelcomeScreen.kt
@@ -1,17 +1,24 @@
 package com.example.questnavigastugas_071.view
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.questnavigastugas_071.R
 
 @Composable
 fun WelcomeScreen(navController: NavHostController) {
@@ -23,6 +30,13 @@ fun WelcomeScreen(navController: NavHostController) {
         ) {
             Text("Selamat Datang", fontSize = 28.sp, fontWeight = FontWeight.Bold)
 
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.cardlist_logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
 
 
         }
